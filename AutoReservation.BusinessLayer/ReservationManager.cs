6123a5aa26a888449aa3ea281ad13370b4ae98b7
sandrosc <sandro@012345.ch>
2017-12-11 19:21:07 +0100

    add exceptions and logic

--- AutoReservation.BusinessLayer/ReservationManager.cs
+++ AutoReservation.BusinessLayer/ReservationManager.cs
@@ -1,8 +1,25 @@
-﻿using AutoReservation.Dal.Entities;
+using AutoReservation.BusinessLayer.Exceptions;
+using AutoReservation.Dal.Entities;
 
 namespace AutoReservation.BusinessLayer
 {
-    public class ReservationManager
-        : ManagerBase<Reservation>
-    {}
+	public class ReservationManager
+			: ManagerBase<Reservation>
+	{
+		public new void Add(Reservation reservation)
+		{
+			if (!reservation.DateRangeIsValid())
+			{
+				throw new InvalidDateRangeException(
+						"Datumsbereich erfüllt die Vorgabe, nach der eine Reservation mindestens einen Tag dauert, nicht.",
+						reservation);
+			}
+			if (!reservation.AutoIsAvailable())
+			{
+				throw new UnavailableAutoException(
+						"Das Auto ist zu diesem Zeitpunkt leider nicht verfügbar.", reservation.Auto);
+			}
+			base.Add(reservation);
+		}
+	}
 }
--- AutoReservation.Dal/Entities/Reservation.cs
+++ AutoReservation.Dal/Entities/Reservation.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -32,3 +33,51 @@ namespace AutoReservation.Dal.Entities
         public virtual Kunde Kunde { get; set; }
     }
 }
+=======
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace AutoReservation.Dal.Entities
+{
+    [Table("Reservation")]
+    public class Reservation
+    {
+        [Key, Column("Id")]
+        public int ReservationsNr { get; set; }
+
+        [Required]
+        public int AutoId { get; set; }
+
+        [Required]
+        public int KundeId { get; set; }
+
+        [Required]
+        public DateTime Von { get; set; }
+
+        [Required]
+        public DateTime Bis { get; set; }
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+
+        [ForeignKey("AutoId")]
+        public virtual Auto Auto { get; set; }
+
+        [ForeignKey("KundeId")]
+        public virtual Kunde Kunde { get; set; }
+        
+        public bool DateRangeIsValid()
+        {
+            return (Von < Bis && (Bis - Von).TotalDays >= 1);
+        }
+
+        public bool AutoIsAvailable()
+        {
+            return Auto.Reservationen.Any(r => r != this && r.Von < Bis && r.Bis > Von);
+        }
+    }
+}
+>>>>>>> Stashed changes
