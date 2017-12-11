cd60b982f38b8a7d8190764a2c6f25d4a621b0c7
Cyril Kyburz <kybi1995@gmail.com>
2017-12-11 15:12:07 +0100

    IAutoReservationService

--- AutoReservation.Common/Interfaces/IAutoReservationService.cs
+++ AutoReservation.Common/Interfaces/IAutoReservationService.cs
@@ -1,8 +1,64 @@
 ﻿using AutoReservation.Common.DataTransferObjects;
+using System.Collections.Generic;
+using System.ServiceModel;
 
 namespace AutoReservation.Common.Interfaces
 {
+    [ServiceContract]
     public interface IAutoReservationService
     {
+        #region Auto
+        [OperationContract]
+        List<AutoDto> SelectAutos();
+
+        [OperationContract]
+        AutoDto SelectAuto(int id);
+
+        [OperationContract]
+        void InsertAuto(AutoDto auto);
+
+        [OperationContract]
+        void UpdateAuto(AutoDto auto);
+
+        [OperationContract]
+        void DeleteAuto(AutoDto auto);
+
+        [OperationContract]
+        bool IsAutoAvaible(int id);
+        #endregion
+
+        #region Kunde
+        [OperationContract]
+        List<KundeDto> SelectKunden();
+
+        [OperationContract]
+        KundeDto SelectKunde(int id);
+
+        [OperationContract]
+        void InsertKunde(KundeDto kunde);
+
+        [OperationContract]
+        void UpdateKunde(KundeDto kunde);
+
+        [OperationContract]
+        void DeleteKunde(KundeDto kunde);
+        #endregion
+
+        #region Reservation
+        [OperationContract]
+        List<ReservationDto> SelectReservationen();
+
+        [OperationContract]
+        KundeDto SelectReservation(int id);
+
+        [OperationContract]
+        void InsertReservation(ReservationDto reservation);
+
+        [OperationContract]
+        void UpdateReservation(ReservationDto reservation);
+
+        [OperationContract]
+        void DeleteReservation(ReservationDto reservation);
+        #endregion
     }
 }
