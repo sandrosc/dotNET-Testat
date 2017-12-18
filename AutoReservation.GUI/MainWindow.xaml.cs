5d52c555666d1570e80770fcb388197b9fb2718e
sandrosc <sandro@012345.ch>
2017-12-18 14:43:05 +0100

    initalize GUI project

---
+++ AutoReservation.GUI/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using AutoReservation.Service.Wcf;
+
+namespace AutoReservation.GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private AutoReservationService _autoReservationService;
+
+        private AutoReservationService AutoReservationService =>
+            _autoReservationService ?? (_autoReservationService = new AutoReservationService());
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            UpdateLists();
+        }
+
+        private void UpdateLists()
+        {
+            DataGridAutos.ItemsSource = AutoReservationService.GetAutos();
+        }
+    }
+}
